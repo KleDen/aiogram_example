8c7ecdb5db0f1611996947b6d14f0c75a6801e99
KleDen <asddsasdasddsasdasddsasd@gmail.com>
2021-10-23 16:08:56 +0300

    update project

--- TelegramBotExampleKeyboard/main.py
+++ TelegramBotExampleKeyboard/main.py
@@ -1,13 +1,15 @@
 """
 minimum viable product
 """
-
+import ast
 import logging
 
 from aiogram import Dispatcher, Bot, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.utils import executor
 
+
+from TelegramBotExampleKeyboard.callback_data import items_callback
 from config import TOKEN
 from items import course
 
@@ -27,7 +29,8 @@ async def start_command(message: types.Message):
 keyboard = InlineKeyboardMarkup(row_width=2,
                                 inline_keyboard=[
                                     [
-                                        InlineKeyboardButton(text="Купить товар", callback_data="buy")
+                                        InlineKeyboardButton(text="Купить товар",
+                                                             callback_data=items_callback.new(item_id=f"{course.id}"))
                                     ],
                                     [
                                         InlineKeyboardButton(text="👍", callback_data="like"),
@@ -38,18 +41,40 @@ keyboard = InlineKeyboardMarkup(row_width=2,
                                     ]
                                 ])
 
-
-#
 @dp.message_handler(commands="items")
 async def items_command(message: types.Message):
     await message.answer_photo(course.photo_url, caption='Курс!', reply_markup=keyboard, )
 
 
-@dp.callback_query_handler(text="buy")
+
+
+@dp.callback_query_handler(text_contains="buy")
 async def item_buying(call: CallbackQuery):
-    await call.answer("Вы отменили эту покупку!")
+    await call.answer(cache_time=60)
+
+    callback_data = call.data
+    logging.info(f"{callback_data=}")
+    logging.info(f"{call=}")
+    test=ast.literal_eval(callback_data)
+
+    item_id = test.get()
+    await call.message.edit_caption(f"Покупай товар номер {item_id}")
     await call.message.edit_reply_markup(reply_markup=None)
-#TODO: Документация EDIT MSG
+
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
+
+
+
+@dp.callback_query_handler(items_callback.filter(item_name="apple"))
+async def buying_apples(call: CallbackQuery, callback_data: dict):
+    await call.answer(cache_time=60)
+
+    # Выведем callback_data и тут, чтобы сравнить с предыдущим вариантом.
+    logging.info(f"{callback_data=}")
+
+    quantity = callback_data.get("quantity")
+    await call.message.answer(f"Вы выбрали купить яблоки. Яблок всего {quantity}. Спасибо.",
+                              reply_markup=apples_keyboard)
